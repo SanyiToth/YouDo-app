feb30f921d2fb31c55a767460adf2abdefd24ca5
Tóth Sándor <52709221+SanyiToth@users.noreply.github.com>
2021-02-06 12:12:30 +0100

    MZ-06--Storing the todos

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,50 +1,78 @@
 let createTodo = document.getElementById("create")
 let list = document.getElementById("todolist")
 let textInput = document.getElementById("newtodo")
-let todoArray = []
+let todoArray = getStoredList()
 
+
+isTodoValid(textInput, createTodo)
+showStoredList()
 
 createTodo.addEventListener("click", function (e) {
     e.preventDefault()
     todoArray.unshift(textInput.value)
+    setStoredList(todoArray)
     showStoredList()
-    textInput.value = ""
 })
 
 textInput.addEventListener("keyup", function (e) {
     e.preventDefault()
-    if (isTodoValid(textInput)) {
-        createTodo.disabled = false
-    } else {
-        createTodo.disabled = true
-    }
+    isTodoValid(textInput, createTodo)
 })
 
 
-function delItem(event) {
+function setStoredList(listArray) {
+    localStorage.setItem('myitems', JSON.stringify(listArray))
+}
+
+
+function getStoredList() {
+    if (localStorage.getItem('myitems') !== null) {
+        return JSON.parse(localStorage.getItem('myitems'))
+    } else {
+        return []
+    }
+}
+
+
+function delItem(event, listArray) {
     const li = event.target.parentElement
     const id = li.dataset.id
-    todoArray = todoArray.filter((item, i) => {
+    const filteredList = getStoredList().filter((item, i) => {
         return i.toString() !== id
     })
+    setStoredList(filteredList)
+    listArray = getStoredList()
     showStoredList()
 }
 
-function showStoredList() {
-    list.innerHTML = ""
-    todoArray.forEach((item, index) => {
-        let newLi = `<li data-id=${index}>${item}<span class="delbtn" onclick="delItem(event)">Delete</span></li>`
-        list.innerHTML += newLi
-    })
+
+function listClear() {
+    return list.innerHTML = ""
 }
 
-function isTodoValid(input) {
+function inputClear() {
+    return textInput.value = ""
+}
+
+function showStoredList() {
+    listClear()
+    getStoredList().forEach((item, index) => {
+        let newLi = `<li data-id=${index}>${item}<span class="delbtn" onclick="delItem(event,todoArray)">Delete</span></li>`
+        list.innerHTML += newLi
+    })
+    inputClear()
+
+}
+
+function isTodoValid(input, button) {
     if (input.value.length < 6) {
         input.classList.add("invalid")
         input.classList.remove("valid")
+        button.disabled = true
     } else {
         input.classList.remove("invalid")
         input.classList.add("valid")
+        button.disabled = false
     }
     return input.value.length >= 6
 }
