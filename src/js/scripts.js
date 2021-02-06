e7930dfbcc0e7805c26e6e05527faab3acbbcf28
Tóth Sándor <52709221+SanyiToth@users.noreply.github.com>
2021-02-06 10:57:44 +0100

    MZ-03--Deleting a todo

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,12 +4,34 @@ console.log("Console log works!");
 let createTodo = document.getElementById("create")
 let list = document.getElementById("todolist")
 let textInput = document.getElementById("newtodo")
+let todoArray = []
 
 
 createTodo.addEventListener("click", function (e) {
     e.preventDefault()
     let newTodo = textInput.value
-    let newLi = `<li>${newTodo}</li>`
-    list.innerHTML += newLi
-    textInput.value=""
+    if (newTodo) {
+        todoArray.unshift(newTodo)
+        showStoredList()
+    }
+    textInput.value = ""
 })
+
+
+function delItem(event) {
+    const li = event.target.parentElement
+    const id = li.dataset.id
+    const filteredList = todoArray.filter((item, i) => {
+        return i.toString() !== id
+    })
+    todoArray = filteredList
+    showStoredList()
+}
+
+function showStoredList() {
+    list.innerHTML = ""
+    todoArray.forEach((item, index) => {
+        let newLi = `<li data-id=${index}>${item}<span class="delbtn" onclick="delItem(event)">Delete</span></li>`
+        list.innerHTML += newLi
+    })
+}
