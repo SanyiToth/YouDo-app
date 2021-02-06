517a890710d466cd822156d523dbd1b71dfe453b
Tóth Sándor <52709221+SanyiToth@users.noreply.github.com>
2021-02-06 10:00:26 +0100

    MZ-02--Adding todo to the list

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,2 +1,15 @@
 //can be deleted
 console.log("Console log works!");
+
+let createTodo = document.getElementById("create")
+let list = document.getElementById("todolist")
+let textInput = document.getElementById("newtodo")
+
+
+createTodo.addEventListener("click", function (e) {
+    e.preventDefault()
+    let newTodo = textInput.value
+    let newLi = `<li>${newTodo}</li>`
+    list.innerHTML += newLi
+    textInput.value=""
+})
